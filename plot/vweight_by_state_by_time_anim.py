1d7b80cb704b7314121ce36514e5b8bd67078db0
Andy Shirey <andyshirey@ilovelamp.local>
2020-11-19 22:44:46 -0800

    filebuilder and animation plot changes attempting to streamline and support all static plot visualization features

--- plot/vweight_by_state_by_time_anim.py
+++ plot/vweight_by_state_by_time_anim.py
@@ -3,8 +3,10 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+from electoralytics.metadata import PIVOT_ON_YEAR_CSV, TOTALS_BY_YEAR_CSV
 
-# constant metadata
+
+# metadata
 COL_ABBREV = 'Abbrev'
 COL_STATE = 'State'
 COL_GROUP = 'Group'
@@ -14,31 +16,52 @@ COL_VOTES_COUNTED = 'Votes counted'
 COL_VOTE_WEIGHT = 'Vote weight'
 COL_AVG_WEIGHT = 'Average weight'
 COL_PARTY = 'Party'
+COL_POP_PER_EC = 'Population per EC vote'
+COL_STATE_COUNT = 'State count'
+COL_MOST_EC_VOTES = 'Most EC votes'
+COL_MEAN_POP_AT_MOST_EC = 'Mean pop at most EC votes'
 
 groups = pd.Series(np.array(['Small', 'Confederate', 'Border', 'Northeast', 'Midwest', 'West', 'Total']))
-category_orders = {COL_GROUP: ['Small','Confederate','Border','Northeast','Midwest','West']}
-color_discrete_sequence = ['Gold','Red','DarkSalmon','MediumBlue','Cyan','SpringGreen']
+category_orders = {COL_GROUP: ['Small', 'Confederate', 'Border', 'Northeast', 'Midwest', 'West']}
+color_discrete_sequence = ['Gold', 'Red', 'DarkSalmon', 'MediumBlue', 'Cyan', 'SpringGreen']
 
 # load source data 
-PIVOT_ON_YEAR_CSV = '/Users/andyshirey/Documents/dev/mappingProjects/electoralResults/csv/pivotOnYear.csv'
 pivot_on_year = pd.read_csv(PIVOT_ON_YEAR_CSV)
 pivot_on_year.drop('Unnamed: 0', axis=1, inplace=True)
 
-TOTALS_BY_YEAR_CSV = '/Users/andyshirey/Documents/dev/mappingProjects/electoralResults/csv/totalsByYear.csv'
 totals_by_year = pd.read_csv(TOTALS_BY_YEAR_CSV)
 totals_by_year.drop('Unnamed: 0', axis=1, inplace=True)
+
+# break source data up into 3 eras
+pivot_on_year_1 = pivot_on_year.loc[pivot_on_year[COL_YEAR] <= 1876]
+pivot_on_year_2 = pivot_on_year.loc[pivot_on_year[COL_YEAR] >= 1868].loc[pivot_on_year[COL_YEAR] <= 1976]
+pivot_on_year_3 = pivot_on_year.loc[pivot_on_year[COL_YEAR] >= 1956]
+
+# calculate COL_MEAN_POP_AT_MOST_EC
+totals_by_year[COL_MEAN_POP_AT_MOST_EC] = totals_by_year[COL_MOST_EC_VOTES] * totals_by_year[COL_POP_PER_EC]
+totals_by_year_1 = totals_by_year.loc[totals_by_year[COL_YEAR] <= 1876]
+totals_by_year_2 = totals_by_year.loc[totals_by_year[COL_YEAR] >= 1868].loc[pivot_on_year[COL_YEAR] <= 1976]
+totals_by_year_3 = totals_by_year.loc[totals_by_year[COL_YEAR] >= 1956]
 
 # scatter 1
 fig_title = 'Voter impact by state in presidential elections'
 
-fig = px.scatter(pivot_on_year, x=COL_VOTES_COUNTED, y=COL_EC_VOTES, animation_frame=COL_YEAR, 
+df = pivot_on_year_1
+totals = totals_by_year_1
+
+ec_max = round(df[COL_EC_VOTES].max() * 1.1)
+pop_max = round(df[COL_VOTES_COUNTED].max() * 1.1)
+
+fig = px.scatter(df, x=COL_VOTES_COUNTED, y=COL_EC_VOTES, animation_frame=COL_YEAR, 
                  animation_group=COL_STATE, color=COL_GROUP, hover_name=COL_STATE, title=fig_title,
 #                  category_orders=category_orders, color_discrete_sequence=color_discrete_sequence,
-#                  log_y=True, 
-                 width=1000, height=800, opacity=0.7, 
-                 range_y=[0,55], range_x=[0,10000000])
+                 width=1000, height=800, opacity=0.7, range_y=[0,ec_max], range_x=[0,pop_max])
     
 fig.update_traces(marker=dict(size=12, line=dict(width=1, color='DarkSlateGrey')), selector=dict(mode='markers'))
+
+# nationwide mean line (DERP)
+# fig.add_trace(go.Scatter(x=totals[COL_MEAN_POP_AT_MOST_EC], y=totals[COL_MOST_EC_VOTES], 
+#                          mode='lines', name='Nationwide mean', line=dict(color='black', width=1)))
 
 # fig.add_trace(go.Scatter(x=totals_by_year[COL_VOTES_COUNTED], y=totals_by_year[COL_EC_VOTES], 
 #                          animation_frame=COL_YEAR, animation_group=COL_STATE,
@@ -50,15 +73,18 @@ fig.update_layout(title_x=0.45)
 fig.show()
 
 # scatter 2
-fig = px.scatter(pivot_on_year, x=COL_EC_VOTES, y=COL_VOTE_WEIGHT, animation_frame=COL_YEAR, 
+df = pivot_on_year_1
+ec_max = round(df[COL_EC_VOTES].max() * 1.1)
+weight_min = df[df[COL_VOTE_WEIGHT] > 0][COL_VOTE_WEIGHT].min() * 0.9
+weight_max = df[COL_VOTE_WEIGHT].max() * 1.1
+
+fig = px.scatter(df, x=COL_EC_VOTES, y=COL_VOTE_WEIGHT, animation_frame=COL_YEAR, 
                  animation_group=COL_STATE, size=COL_VOTES_COUNTED, color=COL_GROUP, hover_name=COL_STATE, 
 #                  category_orders=category_orders, color_discrete_sequence=color_discrete_sequence,
                  title=fig_title, width=1000, height=800, opacity=0.5,
-#                  range_y=[0,10],
-                 log_y=True, size_max=80, range_x=[0,55])
+                 log_y=True, size_max=80, range_x=[0,ec_max], range_y=[weight_min,weight_max])
 
-# fig.add_trace(go.Scatter(x=flat_data['EC votes'], y=flat_data['Mean vote weight'], 
-#                          mode='lines', name=trace_name_natl_avg, line=dict(color='black', width=1)))
+fig.add_trace(go.Scatter(x=[0,ec_max], y=[1,1], mode='lines', name='Nationwide mean', line=dict(color='black', width=1)))
 
 fig.update_traces(marker=dict(line=dict(width=1, color='white')),
                   selector=dict(mode='markers'))
--- transform/multi_ring_filebuilder.py
+++ transform/multi_ring_filebuilder.py
@@ -26,22 +26,25 @@ COL_VOTE_WEIGHT = 'Vote weight'
 COL_AVG_WEIGHT = 'Average weight'
 COL_PARTY = 'Party'
 COL_POP_PER_EC = 'Population per EC vote'
+COL_STATE_COUNT = 'State count'
+COL_MOST_EC_VOTES = 'Most EC votes'
 
 # initialize new data frames
-pivot_on_year = pd.DataFrame(columns=[COL_ABBREV, COL_STATE, COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED,
-                                      COL_VOTE_WEIGHT, COL_PARTY])
-group_aggs_by_year = pd.DataFrame(columns=[COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_AVG_WEIGHT])
-totals_by_year = pd.DataFrame(columns=[COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_POP_PER_EC])
+pivot_on_year = pd.DataFrame(
+    columns=[COL_ABBREV, COL_STATE, COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_VOTE_WEIGHT, COL_PARTY])
+group_aggs_by_year = pd.DataFrame(
+    columns=[COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_AVG_WEIGHT, COL_STATE_COUNT])
+totals_by_year = pd.DataFrame(columns=[COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_POP_PER_EC, COL_MOST_EC_VOTES])
 
 ### LEGACY ###
-groups_by_year = pd.DataFrame({COL_GROUP: groups}) 
 avg_weight_by_year = pd.DataFrame({COL_GROUP: groups}) 
-groups_by_year = groups_by_year.set_index(COL_GROUP)
 avg_weight_by_year = avg_weight_by_year.set_index(COL_GROUP)
+# groups_by_year = pd.DataFrame({COL_GROUP: groups}) 
+# groups_by_year = groups_by_year.set_index(COL_GROUP)
 
-## begin iterating through years in the_one_ring
+# begin iterating through years in the_one_ring
 year = 1828
-while year <= 1832:
+while year <= 2016:
 
     # column names for current year
     ec_votes_col = f'{year} {COL_EC_VOTES}'
@@ -51,21 +54,25 @@ while year <= 1832:
     party_col = f'{year} {COL_PARTY}'
 
     # extract electoral college data for year
-    year_data = the_one_ring[[COL_ABBREV, COL_STATE, COL_GROUP, ec_votes_col, votes_counted_col, 
-                              vote_weight_col, party_col]]
-    # set state abbrev as key/index for each row
+    year_data = the_one_ring[
+        [COL_ABBREV, COL_STATE, COL_GROUP, ec_votes_col, votes_counted_col, vote_weight_col, party_col]]
+    # rename columns and set row keys/index
+    year_data.rename(columns={ec_votes_col: COL_EC_VOTES, votes_counted_col: COL_VOTES_COUNTED,
+                              vote_weight_col: COL_VOTE_WEIGHT, party_col: COL_PARTY},
+                    inplace=True)
     year_data = year_data.set_index(COL_ABBREV)
 
+    
     # DATA CLEANSING
     # remove states lacking vote weight for this year 
-    year_data = year_data[pd.notnull(year_data[vote_weight_col])]
+    year_data = year_data[pd.notnull(year_data[COL_VOTE_WEIGHT])]
     
     # explicitly set type of votes counted data to int (not sure why this isn't automatic)
-    year_data[[votes_counted_col]] = year_data[[votes_counted_col]].astype(int)
+    year_data[[COL_VOTES_COUNTED]] = year_data[[COL_VOTES_COUNTED]].astype(int)
 
     # extract US totals data 
-    ec_total = year_data.loc['US'][ec_votes_col]
-    pop_total = year_data.loc['US'][votes_counted_col]
+    ec_total = year_data.loc['US'][COL_EC_VOTES]
+    pop_total = year_data.loc['US'][COL_VOTES_COUNTED]
     # calculate average popular vote per EC vote
     pop_per_ec = round(pop_total / ec_total)
     
@@ -73,75 +80,90 @@ while year <= 1832:
     year_data = year_data.drop('US')
 
     # map states having 4 or fewer electoral college votes to 'Small' Group
-    year_data.loc[year_data[ec_votes_col] <= 4, COL_GROUP] = 'Small'
+    year_data.loc[year_data[COL_EC_VOTES] <= 4, COL_GROUP] = 'Small'
 
 
-    # EXTRACTION: pivot_on_year, totals_by_year
-    # init year_pivot
-    year_pivot = year_data.rename(columns={ec_votes_col: COL_EC_VOTES, votes_counted_col: COL_VOTES_COUNTED, 
-                                           vote_weight_col: COL_VOTE_WEIGHT, party_col: COL_PARTY})
+    # DF 1: assemble year_pivot to append to pivot_on_year
+    year_pivot = year_data
+    # add and populate year column
     year_pivot[COL_YEAR] = [year] * len(year_pivot.index)
-    # unset index so we can append to pivot_on_year
+    # unset index so we can append 
     year_pivot.reset_index(inplace=True)
+    # add placeholder row for any Group that isn't represented 
+    if len(year_pivot.loc[year_pivot['Group'] == 'West']) == 0:
+        year_pivot_bonus = pd.DataFrame([['', '', 'West', year, 0, 0, 0, '']],
+            columns=[COL_ABBREV, COL_STATE, COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, 
+                     COL_VOTE_WEIGHT, COL_PARTY])
+        year_pivot = pd.concat([year_pivot, year_pivot_bonus], ignore_index=True, sort=False)
     # append year_pivot to pivot_on_year
     pivot_on_year = pd.concat([pivot_on_year, year_pivot], ignore_index=True, sort=False)
-    
-    # init year_totals
-    year_totals = pd.DataFrame([[year, ec_total, pop_total, pop_per_ec]],
-                               columns=[COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_POP_PER_EC])
-    # append year_totals to totals_by_year
-    totals_by_year = pd.concat([totals_by_year, year_totals], ignore_index=True, sort=False)
-    
+        
+        
+    # DF 2: init year_totals to append to totals_by_year
+    year_totals = pd.DataFrame([[year, ec_total, pop_total, pop_per_ec, year_data[COL_EC_VOTES].max()]],
+                               columns=[COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_POP_PER_EC, COL_MOST_EC_VOTES])
+    totals_by_year = pd.concat([totals_by_year, year_totals], ignore_index=True, sort=False)  
 
-    # AGGREGATION: group_aggs_by_year
-    # aggregate EC votes and popular votes for each Group, assign summed values to new year_agg dataframe
-    year_group_aggs = year_data.groupby(COL_GROUP).agg({ec_votes_col: 'sum', votes_counted_col: 'sum'})
+    
+    # DF 3: aggregate EC votes and popular votes for each Group, assign summed values to new year_agg dataframe
+    year_group_aggs = year_data.groupby(COL_GROUP).agg(
+        {COL_EC_VOTES: 'sum', COL_VOTES_COUNTED: 'sum', COL_STATE: 'count'})
     # unset index, rename columns, add year column
     year_group_aggs.reset_index(inplace=True)
-    year_group_aggs.rename(columns={ec_votes_col: COL_EC_VOTES, votes_counted_col: COL_VOTES_COUNTED}, inplace=True)
+    year_group_aggs.rename(columns={COL_STATE: COL_STATE_COUNT}, inplace=True)
     year_group_aggs[COL_YEAR] = [year] * len(year_group_aggs)
     # add average weight column by dividing EC votes by popular votes and multiplying by national pop-per-EC factor
     year_group_aggs[COL_AVG_WEIGHT] = pop_per_ec * year_group_aggs[COL_EC_VOTES] / year_group_aggs[COL_VOTES_COUNTED]
+    # add placeholder row for any Group that isn't represented 
+    if len(year_group_aggs.loc[year_group_aggs['Group'] == 'West']) == 0:
+        year_group_aggs_bonus = pd.DataFrame([['West', year, 0, 0, 0, 0]],
+            columns=[COL_GROUP, COL_YEAR, COL_EC_VOTES, COL_VOTES_COUNTED, COL_AVG_WEIGHT, COL_STATE_COUNT])
+        year_group_aggs = pd.concat([year_group_aggs, year_group_aggs_bonus], ignore_index=True, sort=False)
     # append year_group_aggs to group_aggs_by_year
     group_aggs_by_year = pd.concat([group_aggs_by_year, year_group_aggs], ignore_index=True, sort=False)
     
     
+    
+    
     ### LEGACY ###
     # aggregate EC votes and popular votes for each Group, assign summed values to new year_agg dataframe
-    year_agg = year_data.groupby(COL_GROUP).agg({ec_votes_col: 'sum', votes_counted_col: 'sum'})
+    year_agg = year_data.groupby(COL_GROUP).agg({COL_EC_VOTES: 'sum', COL_VOTES_COUNTED: 'sum'})
     # add Average weight column by dividing EC votes by popular votes and multiplying by national pop-per-EC factor
-    year_agg[avg_weight_col] = pop_per_ec * year_agg[ec_votes_col] / year_agg[votes_counted_col]
+    year_agg[year] = pop_per_ec * year_agg[COL_EC_VOTES] / year_agg[COL_VOTES_COUNTED]
 
     # add Total row to end of of year_agg
     total_row = pd.DataFrame([['Total', ec_total, pop_total, 1.0]], 
-                             columns=[COL_GROUP, ec_votes_col, votes_counted_col, avg_weight_col])
+                             columns=[COL_GROUP, COL_EC_VOTES, COL_VOTES_COUNTED, year])
     total_row = total_row.set_index(COL_GROUP)
     year_agg = year_agg.append(total_row)
 
     # combine year_agg into groups_by_year
     #pd.concat([groups_by_year, year_agg], axis=1)
     #pd.merge(groups_by_year, year_agg, left_index=True, right_index=True, how='outer')
-    groups_by_year = groups_by_year.join(year_agg, how='outer')
+    #groups_by_year = groups_by_year.join(year_agg, how='outer')
 
     # extract avg_weight column into its own dataframe, rename column to be simply the year
-    avg_weight_df = year_agg.drop([ec_votes_col, votes_counted_col], axis=1)
-    avg_weight_df = avg_weight_df.rename(columns={avg_weight_col: year})
+    avg_weight_df = year_agg.drop([COL_EC_VOTES, COL_VOTES_COUNTED], axis=1)
     avg_weight_by_year = avg_weight_by_year.join(avg_weight_df, how='outer')
 
+    
+    
+    
     
     # increment to next election
     year = year + 4
 
-    
+
 # write pivot_on_year, totals_by_year, group_aggs_by_year to file
 pivot_on_year.to_csv(PIVOT_ON_YEAR_CSV)
 totals_by_year.to_csv(TOTALS_BY_YEAR_CSV)
 group_aggs_by_year.to_csv(GROUP_AGGS_BY_YEAR_CSV)
+
 
 ### LEGACY ###
 # write groups_by_year to file
 #groups_by_year.to_csv(GROUPS_BY_YEAR_CSV)
 
 # rename Total column and write avg_weight_by_year to file
-#avg_weight_by_year = avg_weight_by_year.rename(index={'Total': 'Nat\'l Average'})
-#avg_weight_by_year.to_csv(AVG_WEIGHT_BY_YEAR_CSV)
+avg_weight_by_year = avg_weight_by_year.rename(index={'Total': 'Nat\'l Average'})
+avg_weight_by_year.to_csv(AVG_WEIGHT_BY_YEAR_CSV)
