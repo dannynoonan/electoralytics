d7655b46d0396e4d2e0b55f1fa823e9a6627a0e5
Andy Shirey <andyshirey@ilovelamp.local>
2020-12-29 08:46:58 -0800

    fiddling with rectangles and annotations denoting eras in line chart and with ways to remove lines for Postbellum and West groups before those groups exist

--- fig_builder/line_charts.py
+++ fig_builder/line_charts.py
@@ -15,6 +15,10 @@ def build_ivw_by_state_group_line_chart(data_obj, groups_dir, max_small, frame, 
     group_aggs_by_year_df = data_obj.group_aggs_by_year_dfs[subdir]
     groups = GROUPS_FOR_DIR[groups_dir]
 
+    # hackish way to remove years where Average Weight is 0 from Postbellum and West Groups without removing it for Confederate or South Groups
+    group_aggs_by_year_df = group_aggs_by_year_df.loc[~((group_aggs_by_year_df[cols.AVG_WEIGHT] == 0) & (group_aggs_by_year_df[cols.GROUP] == 'Postbellum'))]
+    group_aggs_by_year_df = group_aggs_by_year_df.loc[~((group_aggs_by_year_df[cols.AVG_WEIGHT] == 0) & (group_aggs_by_year_df[cols.GROUP] == 'West'))]
+
     if not fig_width:
         fig_width = fig_dims.MD6
     fig_height = 500
@@ -29,10 +33,15 @@ def build_ivw_by_state_group_line_chart(data_obj, groups_dir, max_small, frame, 
     fig = px.line(group_aggs_by_year_df, x=cols.YEAR, y=cols.AVG_WEIGHT, color=cols.GROUP, 
                     hover_name=cols.GROUP, hover_data=hover_data, 
                     color_discrete_map=GROUP_COLORS, category_orders={cols.GROUP: groups},
-                    title=fig_title, width=fig_width, height=fig_height,
+                    title=fig_title, width=fig_width, height=fig_height, 
+                    line_shape='spline',
     #               log_y=True
                 )
+    
+    for i in range(len(fig.data)):
+        fig.data[i].update(mode='markers+lines')
 
+    fig.update_layout(xaxis_range=[1828, 2020])
     fig.update_layout(yaxis_range=[avg_weight_min, avg_weight_max])
 
     # axis labels
@@ -44,56 +53,68 @@ def build_ivw_by_state_group_line_chart(data_obj, groups_dir, max_small, frame, 
     #                         mode='lines', name=frame, line=dict(color='pink', width=1)))
 
 
-    events = {
-        # '1857': 'Dred Scott v Sandford',
-        '1861': 'Civil War begins', 
-        # '1863': ['Emancipation Proclamation',
-        '1865': 'Civil War ends, 13th Amendment ratified',
-        # '1868': '14th Amendment ratified',
-        '1870': '15th Amendment ratified',
-        '1877': 'Compromise of 1877 ends Reconstruction',
-        '1896': 'Plessy v Ferguson',
-        '1920': '19th Amendment ratified',
-        # '1921': 'Tulsa race massacre',
-        '1924': 'KKK peak membership',
-        # '1932': 'Tuskegee Syphilis Study',
-        '1954': 'Brown v Board of Education',
-        # '1955': 'Emmett Till lynching, Montgomery bus boycott',
-        '1960': 'Sit-ins',
-        # '1964': 'Civil Rights Act, 24th Amendment ratified', 
-        '1965': 'Voting Rights Act',
-        '1971': '26th Amendment ratified',
-        '2013': 'Shelby County v. Holder',
-    }
+    events = [
+        # {'year': 1857, 'name': 'Dred Scott v Sandford'},
+        {'year': 1861, 'name': 'Civil War begins'},
+        # {'year': 1863, 'name': 'Emancipation Proclamation'},
+        {'year': 1865, 'name': 'Civil War ends, 13th Amendment'},
+        # {'year': 1868, 'name': '14th Amendment'},
+        {'year': 1870, 'name': '15th Amendment'},
+        {'year': 1877, 'name': 'Compromise of 1877'},
+        {'year': 1896, 'name': 'Plessy v Ferguson'},
+        {'year': 1920, 'name': '19th Amendment'},
+        # {'year': 1921, 'name': 'Tulsa race massacre'},
+        {'year': 1924, 'name': 'KKK peak membership'},
+        # {'year': 1932, 'name': 'Tuskegee Syphilis Study'},
+        {'year': 1954, 'name': 'Brown v Board of Education'},
+        # {'year': 1955, 'name': 'Emmett Till lynching, Montgomery bus boycott'},
+        {'year': 1960, 'name': 'Sit-ins'},
+        # {'year': 1964, 'name': 'Civil Rights Act, 24th Amendment'}, 
+        {'year': 1965, 'name': 'Voting Rights Act'},
+        {'year': 1971, 'name': '26th Amendment'},
+        {'year': 2013, 'name': 'Shelby County v. Holder'},
+    ]
 
     eras = [
-        {'begin': 1828, 'end': 1861, 'name': 'Antebellum Period'},
-        {'begin': 1861, 'end': 1865, 'name': 'Civil War'},
-        {'begin': 1865, 'end': 1877, 'name': 'Reconstruction'},
-        {'begin': 1877, 'end': 1896, 'name': 'Redemption / Early Jim Crow'},
-        {'begin': 1896, 'end': 1954, 'name': 'Jim Crow'},
-        {'begin': 1954, 'end': 1965, 'name': 'Late Jim Crow / Civil Rights Era'},
-        {'begin': 1965, 'end': 2020, 'name': 'Post Voting Rights Act'},
+        {'begin': 1828, 'end': 1861, 'name': 'Antebellum Period', 'color': '#636EFA'},
+        {'begin': 1861, 'end': 1865, 'name': 'Civil War', 'color': '#FECB52'},
+        {'begin': 1865, 'end': 1877, 'name': 'Reconstruction', 'color': '#00CC96'},
+        {'begin': 1877, 'end': 1896, 'name': 'Redemption', 'color': '#AB63FA'},
+        {'begin': 1896, 'end': 1954, 'name': 'Jim Crow', 'color': '#FFA15A'},
+        {'begin': 1954, 'end': 1965, 'name': 'Civil Rights Era', 'color': '#19D3F3'},
+        {'begin': 1965, 'end': 2020, 'name': 'Post Voting Rights Act', 'color': '#FF6692'},
     ]
 
     # build markers and labels marking events 
     event_markers = []
-    for year, event in events.items():
-        year = int(year)
+    for event in events:
         # add vertical line for each event date
-        marker = dict(type='line', line_width=1, x0=year, x1=year, y0=avg_weight_min, y1=avg_weight_max)
+        marker = dict(type='line', line_width=1, x0=event['year'], x1=event['year'], y0=avg_weight_min, y1=avg_weight_max)
         event_markers.append(marker)
         # add annotation for each event
-        fig.add_annotation(x=year-1, y=avg_weight_max, text=event, showarrow=False, 
+        fig.add_annotation(x=event['year']-1, y=avg_weight_max, text=event['name'], showarrow=False, 
             yshift=-2, xshift=-1, 
             textangle=-90, align='right', yanchor='top')
 
     # build shaded blocks designating eras
     era_blocks = []
-    for i in range(len(eras)):
-        block = dict(type='rect', line_width=0, x0=eras[i]['begin'], x1=eras[i]['end'], y0=avg_weight_min, y1=avg_weight_max, 
-                    fillcolor=COLORS_PLOTLY[i], opacity=0.1)
+    for era in eras:
+        # add rectangle for each era date range
+        block = dict(type='rect', line_width=0, x0=era['begin'], x1=era['end'], y0=avg_weight_min, y1=avg_weight_max, 
+                    fillcolor=era['color'], opacity=0.1)
         era_blocks.append(block) 
+        # add annotation for each era
+        era_len = era['end'] - era['begin']
+        era_mid = (era['end'] + era['begin']) / 2
+        showarrow = False
+        yshift = 7
+        if era_len < 12:
+            showarrow = True
+            yshift = 0
+        
+        fig.add_annotation(x=era_mid, y=avg_weight_max, text=era['name'], showarrow=showarrow, yshift=yshift, 
+            # align='left', textangle=-90, yanchor='top'
+        )
 
     # update layout with era_blocks and event_markers
     fig.update_layout(shapes=era_blocks + event_markers)
