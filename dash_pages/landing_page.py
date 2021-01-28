6910b4c80bb10d2eae2e4a9c72c8630955489f88
Andy Shirey <andyshirey@ilovelamp.local>
2021-01-27 21:13:21 -0800

    content edits to conclusions page, formatting tweaks on landing page

--- dash_pages/landing_page.py
+++ dash_pages/landing_page.py
@@ -19,45 +19,45 @@ content = html.Div([
                 html.H4(className="mb-1 text-success", children=[
                     "Using “Voter Weight” to Examine Electoral College Bias (main article)"
                 ]),
-                html.Img(src="/static/screenshots/barFreeSlaveSmall1852.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/mapFreeSlaveSmall1852.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/scatterDotsAcw1920.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/scatterBubblesAcw1940.png", height="250", style={"padding": "5px"}),
+                html.Img(src="/static/screenshots/barFreeSlaveSmall1852.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/mapFreeSlaveSmall1852.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/scatterDotsAcw1920.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/scatterBubblesAcw1940.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
             ]),
             html.A(className="list-group-item list-group-item-action flex-column align-items-start bg-primary", 
                 href="/voter-weight-timeline-visualization", children=[
                 html.H4(className="mb-1 text-success", children=[
                     "Annotated Timeline Charting Voter Weight Trends: 1800 - 2020",
                 ]),
-                html.Img(src="/static/screenshots/lineChartGroupVwSince1800.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/lineChartWIvsGAsince1800.png", height="250", style={"padding": "5px"}),
+                html.Img(src="/static/screenshots/lineChartGroupVwSince1800.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/lineChartWIvsGAsince1800.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
             ]),
             html.A(className="list-group-item list-group-item-action flex-column align-items-start bg-primary", 
                 href="/voter-weight-figure-vault", children=[
                 html.H4(className="mb-1 text-success", children=[
                     "The Vault: 220 Years of Maps, Bar Charts, Scatter Plots, and Box Plots",
                 ]),
-                html.Img(src="/static/screenshots/scatterBubblesAcw1900.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/boxAcw1920.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/scatterAbbrevsAcw1920.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/mapVoteWeights1880.png", height="250", style={"padding": "5px"}),
+                html.Img(src="/static/screenshots/scatterBubblesAcw1900.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/boxAcw1920.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/scatterAbbrevsAcw1920.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/mapVoteWeights1880.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
             ]),
             html.A(className="list-group-item list-group-item-action flex-column align-items-start bg-primary", 
                 href="/voter-weight-calculation", children=[
                 html.H4(className="mb-1 text-success", children=[
                     "Calculating Voter Weight",
                 ]),
-                html.Img(src="/static/vwMath/derivingPvpeVw.png", height="150", style={"padding": "5px"}),
-                html.Img(src="/static/vwMath/vwEcSummQuotEquation.png", height="150", style={"padding": "5px"}),
-                html.Img(src="/static/vwMath/vwMathSampler/popToEcWIGA1900.png", height="150", style={"padding": "5px"}),
+                html.Img(src="/static/vwMath/derivingPvpeVw.png", height="150", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/vwMath/vwEcSummQuotEquation.png", height="150", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/vwMath/vwMathSampler/popToEcWIGA1900.png", height="150", style={"padding": "5px", "padding-top": "10px"}),
             ]),
             html.A(className="list-group-item list-group-item-action flex-column align-items-start bg-primary", 
                 href="/explanation-of-groupings", children=[
                 html.H4(className="mb-1 text-success", children=[
                     "Explanation of State Groupings",
                 ]),
-                html.Img(src="/static/screenshots/mapAcw1880.png", height="250", style={"padding": "5px"}),
-                html.Img(src="/static/screenshots/mapCensus1960.png", height="250", style={"padding": "5px"}),
+                html.Img(src="/static/screenshots/mapAcw1880.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
+                html.Img(src="/static/screenshots/mapCensus1960.png", height="250", style={"padding": "5px", "padding-top": "10px"}),
             ]),
             html.A(className="list-group-item list-group-item-action flex-column align-items-start bg-primary", 
                 href="/voter-weight-conclusions", children=[
--- dash_pages/voter_weight_conclusions.py
+++ dash_pages/voter_weight_conclusions.py
@@ -22,27 +22,27 @@ content = html.Div([
                         dbc.CardBody([
                             html.H4("Passing the eye test", className="card-title"),
                             html.P(className="card-text", children=[
-                                "Being neither a historian nor a statistician, there probably isn’t much I should presume to “conclude” from these data \
-                                visualizations. My idea here was to use some new tech to breathe life into a data set, try to validate a surprising claim \
-                                I’d heard on a podcast, and ultimately see if visualizing the data would point to any other conclusions, effectively \
-                                letting it tell its own story."
+                                "Being neither a historian nor a statistician, there probably isn’t much I should presume to “conclude” from this \
+                                assortment of data visualizations. As a software engineer (with a budding interest in racial justice history) my idea \
+                                here was to use some new tech to breathe life into an old data set, try to validate a surprising claim I’d heard on a \
+                                podcast, and ultimately see if visualizing the data would point to any other conclusions, effectively letting it tell \
+                                its own story."
                             ]),
                             html.P(className="card-text", children=[
-                                "And sure enough: the slave-state bias is plain to see in the would-be Confederate (and Border) states up till the Civil \
-                                War, the small-state bias is plain to see all the way from the founding to the present, and what I’m calling a \
-                                suppression-state bias is plain to see during the period between the end of Reconstruction in 1877 and the passage of the \
-                                Voting Rights Act in 1965."
+                                "And sure enough: the slave-state bias is plain to see in the slave states leading up to the Civil War, the small-state \
+                                bias is plain to see all the way from the founding to the present, and what I’m calling a suppression-state bias is \
+                                plain to see during the period between the end of Reconstruction in 1877 and the passage of the Voting Rights Act in 1965."
                             ]),
                             html.P(className="card-text", children=[
                                 "In the two decades following Reconstruction, the voter suppression effect in former Confederate states appears to have \
-                                ticked up to levels matching the antebellum Voter Weight status quo, followed by a period of significant amplification \
-                                during the first five decades of the 20th century."
+                                ticked up to levels matching the antebellum Voter Weight status quo, followed by a period of ", html.I("significant"),
+                                " amplification during the first five decades of the 20th century."
                             ]),
                             html.P(className="card-text", children=[
                                 "These aren’t rigorously-defensible scholarly claims, they’re layperson interpretations of a data set that’s much easier \
-                                to examine in two-dimensional interactive color than in rows and columns. That said, I’d wager every inflection point in \
-                                any of the figures links back to some specific set of historic events, the sort of thing that a historian might look at \
-                                and immediately say “oh right — yeah that sudden change in X is probably a side-effect of Y.”"
+                                to examine in two-dimensional interactive color than in rows and columns. Nonetheless, I’d wager many of the inflection \
+                                points in the figures links back to some specific set of historic events, the sort of thing that a historian might look at \
+                                and immediately say “oh right — yeah that sudden change in X could easily be a side-effect of Y.”"
                             ]),
                             html.Br(),
                             html.H4("A few things that stood out", className="card-title"),
@@ -50,18 +50,21 @@ content = html.Div([
                                 "It’s been interesting pausing to research events and periods along the way, attempting to interpret underlying causes of \
                                 twists and turns in the data. Here’s a short list of the many data ripples that I initially mistook for errors, anomalies, \
                                 or unexplained distortions:",
-                                html.Li("the impact of the “Mississippi Plan,” a scheme to introduce discriminatory voting language into every Southern \
-                                state’s constitution, beginning in 1890"),
-                                html.Li("the staggered state-by-state expansion of women’s suffrage in the early 20th century, prior to the 19th \
-                                Amendment’s ratification in 1920"),
-                                html.Li("the “high watermark” of suppression-state bias in the election of 1924 and its proximity to events like the Tulsa \
-                                massacre in 1921 and peak KKK membership in 1924"),
-                                html.Li("the erratic differences in popular voter participation from state to state and year to year in the pre-Jacksonian \
-                                era, as states experimented with everything from state-wide popular vote to district-level selection to electors being \
-                                appointed by legislatures"),
-                                html.Li("the cyclical effect of the decennial census and of EC reapportionment catching up to population swells"),
-                                html.Li("a 120-year uninterrupted streak where Midwestern states appear to have been more democratic than their \
-                                Northeastern neighbors (toggle to the “Regional Census” groupings on the electoralytics site to see)"),
+                                html.Ul([
+                                    html.Li("the impact of the “Mississippi Plan,” a scheme to introduce discriminatory voting language into every Southern \
+                                    state’s constitution, beginning in the 1890s"),
+                                    html.Li("the staggered state-by-state expansion of women’s suffrage (and therefore voter turnout overall) in the early \
+                                    20th century, prior to the 19th Amendment’s ratification enfranchising women in every state in 1920"),
+                                    html.Li("the “high watermark” of suppression-state bias in the election of 1924 and its proximity to events like the \
+                                    Tulsa Race Massacre in 1921, peak KKK membership in 1924, and the segregationist presidency of Woodrow Wilson"),
+                                    html.Li("the erratic differences in popular voter participation from state to state and year to year in the pre-Jacksonian \
+                                    era, as states experimented with everything from state-wide popular vote to district-level selection to electors being \
+                                    appointed by legislatures"),
+                                    html.Li("the cyclical effect of the decennial census and of Electoral College reapportionment catching up to population \
+                                    swells"),
+                                    html.Li("a curious 120-year uninterrupted streak where Midwestern states appear to have been more democratic than their \
+                                    Northeastern neighbors (toggle to the “Regional Census” groupings to see, it still has me baffled)"),
+                                ]),
                             ]),
                             html.Br(),
                             html.H4("Border states", className="card-title"),
@@ -73,18 +76,18 @@ content = html.Div([
                             ]),
                             html.P(className="card-text", children=[
                                 "At the moment, my knowledge of Reconstruction, Redemption, Reconciliation, and Jim Crow is almost entirely limited to a \
-                                North vs South narrative, excluding the details of Border and Western states. ", html.I("That said:"), " knowing little to \
-                                nothing of the ", html.I("details"), " of post-Reconstruction enfranchisement practices in Border states, it sure looks to \
-                                me as though those practices led to much higher voter participation (and, by inverse relationship, lower influence of \
-                                individual voters) than that of other slave states that had fought with the Confederacy."
+                                North vs South narrative, excluding the details of Border and Western states. Caveats aside: knowing little to nothing of \
+                                the ", html.I("details"), " of post-Reconstruction enfranchisement practices in Border states, it sure looks to me as \
+                                though those practices led to much higher voter participation (and, by inverse relationship, lower influence of individual \
+                                voters) than that of Confederate slave states."
                             ]),
                             html.P(className="card-text", children=[
-                                "From the 40,000’ view enabled by these data visualizations, post-war voter turnout in Union-loyal former-slave states \
-                                appears almost indistinguishable from voter participation in Union-loyal free states. I’d be reaching way out of my lane to \
-                                interpret this here, but suffice to say I think this has fascinating implications."
+                                "In fact, from the 40,000’ view enabled by these data visualizations, post-war voter turnout in Union-loyal former-slave \
+                                states appears almost indistinguishable from voter participation in Union-loyal free states. I’d be reaching way out of my \
+                                lane to interpret this here, but suffice to say I think this has fascinating implications."
                             ]),
                             html.P(className="card-text", children=[
-                                "One final comment, with respect to demographics. It’s true that, when the Civil War began, Union-loyal Border states like \
+                                "One last thought, with respect to demographics. It’s true that, when the Civil War began, Union-loyal Border states like \
                                 Kentucky, Maryland, and Missouri had somewhat fewer slaves per capita than their Confederate counterparts to the south. But \
                                 these were not anecdotally slave “ish” states: more than 425K Blacks were enslaved in these three states per the ",
                                 dcc.Link("1860 census", href="https://www.census.gov/history/pdf/ApportionmentInformation-1860Census.pdf", target="_blank"), 
@@ -93,8 +96,11 @@ content = html.Div([
                             html.P(className="card-text", children=[
                                 "In absolute terms, Kentucky had more people enslaved than Texas, Missouri had more slaves than Arkansas, and Maryland had \
                                 more slaves than Florida. Yet the differences in the voter enfranchisement practices in these states a full 100 years after \
-                                the abolition of slavery couldn’t be more stark. Post-Reconstruction voter disenfranchisement seems to have had less to do \
-                                with a state’s connection to slavery than to the side it fought with in the Civil War."
+                                the abolition of slavery couldn’t be more stark."
+                            ]),
+                            html.P(className="card-text", children=[
+                                html.B("Bottom line:"), html.I(" post-Reconstruction voter disenfranchisement seems to have had less to do with a state’s historic connection to \
+                                slavery than to the side it fought with in the Civil War."),
                             ]),
                         ]),
                     ]),
@@ -107,15 +113,17 @@ content = html.Div([
                                 "To avoid wrapping every comment in a caveat, I’ve been fairly loose in my pronouncements that higher Voter Weights correlate \
                                 to higher levels of voter suppression, so I wanted to rattle off a few things that most certainly also have an effect on a \
                                 state’s Voter Weight metric:",
-                                html.Li("Voter apathy, and hence lower voter participation. Since the “solid south” of the early 20th century was effectively \
-                                    a non-competitive one-party regional bloc (in part due to voter suppression), the fact that down-ballot winners were a \
-                                    foregone conclusion may have further depressed turnout"),
-                                html.Li("Other eligibility factors such as age, gender, incarceration status, felony record, etc vary from state to state"),
-                                html.Li("Census apportionment is decennial, so EC influence always lags a few years behind a population explosion, lowering \
-                                    the Voter Weight in fast-growing states near the end of the decade"),
-                                html.Li("Census apportionment in states with a higher percentage of undocumented residents who can’t legally vote increases \
-                                    Voter Weight in that state (generally regarded as to the benefit, not the detriment, of those who are tabulated but not \
-                                    enfranchised, but a five-fifths type effect nonetheless)"),
+                                html.Ul([
+                                    html.Li("Voter apathy, and hence lower voter participation. Since the “solid south” of the early 20th century was effectively \
+                                        a non-competitive one-party regional bloc (in part due to the wild success of their voter suppression tactics), the fact \
+                                        that down-ballot winners were a foregone conclusion may have further depressed turnout"),
+                                    html.Li("Other eligibility factors such as age, gender, incarceration status, felony record, etc vary from state to state"),
+                                    html.Li("Census apportionment is decennial, so EC influence always lags a few years behind a population explosion, lowering \
+                                        the Voter Weight in fast-growing states near the end of the decade"),
+                                    html.Li("Census apportionment in states with a higher percentage of undocumented residents who can’t legally vote increases \
+                                        Voter Weight in that state. This is generally regarded as to the benefit, not the detriment, of those who are tabulated \
+                                        but not enfranchised, but this creates a “five-fifths” type effect nonetheless."),
+                                ]),
                             ]),
                             html.Br(),
                             html.H4("The Voting Rights Act of 1965", className="card-title"),
@@ -134,22 +142,22 @@ content = html.Div([
                             html.P(className="card-text", children=[
                                 "THAT SAID... anybody who’s intently focused on present-day voting rights issues will be quick to point out that (a) omg there’s \
                                 hella disenfranchisement out there today, worse than ever with regards to felons, voter ID laws, overzealous signature matching, \
-                                precinct-targeted voter roll purges, etc etc, and (b) the Supreme Court’s recent meddling in the Voting Rights Act of 1965 in \
-                                2013’s ", html.I("Shelby County v. Holder"), " ruling actually presents a very real threat to the durability of positive \
+                                precinct-targeted voter roll purges, etc etc, and (b) the Supreme Court’s recent 2013 meddling in the Voting Rights Act of 1965 \
+                                vis-à-vis their ", html.I("Shelby County v. Holder"), " ruling actually presents a very real threat to the durability of positive \
                                 historical trends."
                             ]),
                             html.P(className="card-text", children=[
                                 "Although my quasi-scientific 40,000’ view of historical Voter Weight doesn’t show any obvious reversal of aggregate positive \
                                 trends in 2016 or 2020 (again: not an actual statistician, I just play one on the internet), it’s worth noting that it took a few \
-                                election cycles after Reconstruction for the Redeemer South’s Voter Weight metric to tick back up to pre-war levels - only to \
-                                wildly exceed that antebellum status quo a generation later."
+                                election cycles after Reconstruction for the Redeemer South’s suppression strategies to translate into pre-war Voter Weight \
+                                levels - only to wildly exceed that antebellum status quo over the course of several generations to follow."
                             ]),
                             html.P(className="card-text", children=[
                                 "Moreover, if there’s a lesson I’ve gleaned from reading about the Electoral College and racial justice history, it’s that \
                                 familiar old discriminatory policies tend to continually reinvent themselves, often using familiar old tricks wrapped in thinly \
                                 veiled language. And as long as the inherent asymmetry between population and participation leave the Electoral College \
                                 vulnerable to manipulation, people are going to exploit it to increase their influence at the expense of others, promulgating \
-                                stories about why the imbalance is justified (even patriotic, “as the framers intended,” etc)."
+                                stories about why the imbalance is justified — even patriotic, “as the framers intended,” etc."
                             ]),
                             html.P(className="card-text", children=[
                                 "And as long as the lights remain on in the twisted halls of white supremacy — and oh boy do they seem to be on right now — \
